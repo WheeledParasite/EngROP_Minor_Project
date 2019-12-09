887a1f20cbdd652718e88275262b4eef53e7e93d
Hunter <huntermonster8@gmail.com>
2019-12-08 20:33:53 -0800

    Added more functionality to UDP server

--- Raspberry_Pi/Python/udp_server.py
+++ Raspberry_Pi/Python/udp_server.py
@@ -1,12 +1,18 @@
+# Socket Docs: https://docs.python.org/3.3/library/socket.html#socket.socket.listen
+
 import socket
 import sys
+from subprocess import check_call
 from gpiozero import LED
+
+HOST = ''                 # Symbolic name meaning all available interfaces
+PORT = 61625
 
 # Create a UDP socket
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
 # Bind the socket to the port
-server_address = ('192.168.1.115', 61625)
+server_address = (HOST, PORT)
 print('starting up on {} port {}'.format(*server_address))
 sock.bind(server_address)
 
@@ -20,12 +26,15 @@ while True:
 		len(data), address))
 	print(data)
 
-	if data:
-		sent = sock.sendto(data, address)
-		print('sent {} bytes back to {}'.format(sent, address))
+#	if data:
+#		sent = sock.sendto(data, address)
+#		print('sent {} bytes back to {}'.format(sent, address))
 	if data == b'PING':
 		print ('ping received')
-		led.on()
-	if data == b'OFF':
-		print ('off received')
-		led.off()
+		sent = sock.sendto(b'ping received', address)
+	if data == b'STOP':
+		print ('stop received')
+		sent = sock.sendto(b'stop received', address)
+    if data == b'SHUTDOWN':
+        print ('shutting down')
+        check_call(['sudo','poweroff'])
