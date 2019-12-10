8746fc58915c3acc6ee760cc159b8f6c9b1b0230
Hunter <huntermonster8@gmail.com>
2019-12-09 16:48:01 -0800

    Revert "Compute x and y values from angle and strength"
    This reverts commit 8c9be06f8b57d44c2f93042a8d31507fa40f84bc.

--- Android/HunterCar/app/src/main/java/com/beehiveproductions/huntercar/MainActivity.java
+++ Android/HunterCar/app/src/main/java/com/beehiveproductions/huntercar/MainActivity.java
@@ -6,61 +6,91 @@ import android.content.pm.ActivityInfo;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
+import android.text.method.ScrollingMovementMethod;
+import android.util.Log;
 import android.view.View;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.widget.Button;
+import android.widget.TextView;
 
 import java.io.IOException;
+import java.math.RoundingMode;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
+import java.text.DecimalFormat;
 
 import io.github.controlwear.virtual.joystick.android.JoystickView;
 
 public class MainActivity extends AppCompatActivity {
+
+    final private String RTSPStream = "http://192.168.2.9:8080/stream";  // Raspberry Pi RTSP Server Address
+    float forspeed = 0f;
+    float rotspeed = 0f;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        JoystickView joystick = (JoystickView) findViewById(R.id.JoystickView);
-        Button redButton = (Button) findViewById(R.id.redbutton);
-        Button greenButton = (Button) findViewById(R.id.greenbutton);
+        JoystickView joystick = findViewById(R.id.JoystickView);
+        Button redButton = findViewById(R.id.redbutton);
+        Button greenButton = findViewById(R.id.greenbutton);
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
-        WebView myWebView = (WebView) findViewById(R.id.webview);
+        WebView myWebView = findViewById(R.id.webview);
+        final TextView myTextView = findViewById(R.id.textView);
+        // setup scrollbars for TextView
+        myTextView.setMovementMethod(new ScrollingMovementMethod());
+
+        // This is the workaround needed to get Android to display non HTTPS content!!!
         if (Build.VERSION.SDK_INT >= 21) {
             myWebView.getSettings().setMixedContentMode( WebSettings.MIXED_CONTENT_ALWAYS_ALLOW );
         }
-        myWebView.loadUrl("http://192.168.1.115:8080/stream");
+        myWebView.loadUrl(RTSPStream);
 
+        joystick.setAutoReCenterButton(true);
+        // Joystick Handler
         joystick.setOnMoveListener(new JoystickView.OnMoveListener() {
             @Override
             public void onMove(int angle, int strength) {
                 // do whatever you want
-                sendMessage(String.valueOf(angle)+String.valueOf(strength));
+                //sendMessage(String.valueOf(angle)+String.valueOf(strength), myTextView);
+                //Log.d("Joystick","A: " + angle + " S: " + strength);
+
+                // Ok, the joystick returns an angle and strength, we need to do a little bit of
+                // trigonometry to figure this out!
+                DecimalFormat df = new DecimalFormat("#.##");
+                df.setRoundingMode(RoundingMode.HALF_UP);
+                forspeed = strength / 100f;
+                rotspeed = (float)(Math.cos(Math.toRadians(angle))*strength/100f);
+                sendMessage(df.format(forspeed) + ":" + df.format(rotspeed),myTextView);
+                Log.d("Hunter","FS: " + df.format(forspeed) + " RS: " + df.format(rotspeed));
             }
-        });
+        },150);  // wait 150ms to send next data
+
+
+        // Button Handlers
         redButton.setOnClickListener(new Button.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //sendMessage("R");
+                // send shutdown raspberry pi
+                sendMessage("SHUTDOWN", myTextView);
             }
         });
         greenButton.setOnClickListener(new Button.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //sendMessage("R");
+                sendMessage("STOP", myTextView);
             }
         });
 
     }
 
-
-    private void sendMessage(final String message) {
-
+    // send one UDP message using a Java Thread
+    private void sendMessage(final String message, final TextView mTextViewReplyFromServer) {
         final Handler handler = new Handler();
+        final String UDPServer = "192.168.2.9";  // Raspberry Pi UDP Server
         Thread thread = new Thread(new Runnable() {
 
             String stringData;
@@ -72,7 +102,7 @@ public class MainActivity extends AppCompatActivity {
                 try {
                     ds = new DatagramSocket();
                     // IP Address below is the IP address of that Device where server socket is opened.
-                    InetAddress serverAddr = InetAddress.getByName("192.168.1.115");
+                    InetAddress serverAddr = InetAddress.getByName(UDPServer);
                     DatagramPacket dp;
                     dp = new DatagramPacket(message.getBytes(), message.length(), serverAddr, 61625);
                     ds.send(dp);
@@ -90,13 +120,13 @@ public class MainActivity extends AppCompatActivity {
                     }
                 }
 
+                // get reply from RC Car udp server and display on screen
                 handler.post(new Runnable() {
                     @Override
                     public void run() {
-
-                        //String s = mTextViewReplyFromServer.getText().toString();
-                        //if (stringData.trim().length() != 0)
-                        //    mTextViewReplyFromServer.setText(s + "\nFrom Server : " + stringData);
+                        String s = mTextViewReplyFromServer.getText().toString();
+                        if (stringData.trim().length() != 0)
+                            mTextViewReplyFromServer.setText(s + "\nFrom Server : " + stringData);
 
                     }
                 });
