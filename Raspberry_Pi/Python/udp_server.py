7ac02c2193bece181b2c60b28fdcb4634db3d556
Hunter <huntermonster8@gmail.com>
2019-12-24 15:58:20 -0800

    Added Support for Ultrasound HC-SR04

--- Raspberry_Pi/Python/udp_server.py
+++ Raspberry_Pi/Python/udp_server.py
@@ -17,6 +17,10 @@ import socket
 import sys
 from subprocess import check_call
 from gpiozero import Robot
+from gpiozero import DistanceSensor
+from gpiozero.pins.pigpio import PiGPIOFactory
+from time import sleep
+
 
 HOST = ''                 # Symbolic name meaning all available interfaces
 PORT = 61625
@@ -24,6 +28,9 @@ IN1 = 12
 IN2 = 16
 IN3 = 20
 IN4 = 21
+
+#create pi GPIO pin factory for distance sensing
+factory = PiGPIOFactory()
 
 # Create a UDP socket
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -35,6 +42,28 @@ sock.bind(server_address)
 
 # create a robot
 robot = Robot(left=(IN1,IN2), right=(IN3,IN4))
+
+#create a left  sensor (Pins 5,6)
+leftSensor = DistanceSensor (echo = 5, trigger = 6, pin_factory = factory)
+#create a right sensor (Pins 23,24)
+rightSensor = DistanceSensor(echo = 23, trigger = 24, pin_factory = factory)
+
+leftDistance = 0
+rightDistance = 0
+shutdown = False
+
+def distance_thread(timeout):
+    while not shutdown:
+		leftDistance = sensor.distance*100
+		sleep(timeout)
+		rightDistance = sensor.distance*100
+		sleep(timeout)
+		print('Left distance: ', leftDistance )
+		print('Right distance', rightDistance)
+
+#launch thread
+x = threading.Thread(target=distance_thread, args=(1,))
+x.start()
 
 while True:
 	print('\nwaiting to receive message')
@@ -55,12 +84,19 @@ while True:
 		print ('stop received')
 		sent = sock.sendto(b'stop received', address)
 	elif data == b'SHUTDOWN':
+		shutdown = True
 		robot.stop()
+		x.join()
         sent = sock.sendto(b'Shutting Down Robot', address)
 		print ('shutting down')
 		check_call(['sudo','poweroff'])
 		break  # exit while loop and take no more commands
 	else:
+		#send distance data
+		sent = sock.sendto(b'R:'+str(rightDistance), address)
+		sent = sock.sendto(b'L:' +str(leftDistance), address)
+
+		#recieve data
 		str_data = data.decode("utf-8")
 		new_data = str_data.split(':')
 		print(new_data[0])
